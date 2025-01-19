975fc54d5a1f75f3bcf22aef913bf664e9bd3179
Alptekin Aktaş <107159536+AlptAktas@users.noreply.github.com>
2025-01-20 00:20:39 +0300

    1

--- Day_35/main.py
+++ Day_35/main.py
@@ -1,3 +1,17 @@
-# API keys, Authentication, Environment Variables and Sending SMS
+import requests
+
+URL = "https://api.openweathermap.org/data/2.5/forecast"
+API_KEY = "412a0837570718deb703b7936b6c5042"
 
 
+parameters={
+    "lat": 51.507351,
+    "lon": -0.127758,
+    "appid": API_KEY
+}
+
+
+response = requests.get(url=URL, params=parameters)
+response.raise_for_status()
+
+print(response.json())
